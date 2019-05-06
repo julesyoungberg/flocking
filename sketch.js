c5484fa675eb5ed49f779cad748037e44afa8b53
Jules Youngberg <julesdayoungberg@gmail.com>
2019-05-06 16:35:51 -0700

    added basic control ui but not yet connected

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
 let flock, center
-let running = true
+let paused = false
+let debug = false
 
 // config
-let swt = 25
+let swt = 25.0
 let awt = 4.0
 let cwt = 5.0
 let maxSpeed = 2
 let maxForce = 0.025
+let boidSize = 1.0
 
 function setup() {
 	createCanvas(windowWidth, 600)
@@ -28,12 +30,62 @@ function setup() {
 	}
 
 	smooth()
+
+
+	// WEIGHT CONTROL
+	const swtControl = createInput(swt, 'number')
+	swtControl.position(10, 650)
+	const swtLabel = createElement('label', 'separation weight')
+	swtLabel.position(10, 630)
+
+	const awtControl = createInput(awt, 'number')
+	awtControl.position(150, 650)
+	const awtLabel = createElement('label', 'alignment weight')
+	awtLabel.position(150, 630)
+
+	const cwtControl = createInput(cwt, 'number')
+	cwtControl.position(290, 650)
+	const cwtLabel = createElement('label', 'cohesion weight')
+	cwtLabel.position(290, 630)
+
+	// LIMITS CONTROL
+	const msControl = createInput(maxSpeed, 'number')
+	msControl.position(10, 700)
+	const msLabel = createElement('label', 'max speed')
+	msLabel.position(10, 680)
+
+	const mfControl = createInput(maxForce, 'number')
+	mfControl.position(150, 700)
+	const mfLabel = createElement('label', 'max force')
+	mfLabel.position(150, 680)
+
+	const sizeControl = createInput(boidSize, 'number')
+	sizeControl.position(290, 700)
+	const sizeLabel = createElement('label', 'boid size')
+	sizeLabel.position(290, 680)
+
+	// GENERAL APPLICATION CONTROLS
+	const debugControl = createCheckbox('debug', debug)
+	debugControl.position(10, 730)
+	debugControl.changed(toggleDebugging)
+
+	const pauseControl = createCheckbox('pause', paused)
+	pauseControl.position(150, 730)
+	pauseControl.changed(togglePaused)
+}
+
+function toggleDebugging() {
+	if (this.checked()) debug = true
+	else debug = false
+}
+
+function togglePaused() {
+	if (this.checked()) paused = true
+	else paused = false
 }
 
 function draw() {
-	if (running) {
-		flock.run()
-	}
+	if (!paused) flock.run()
 }
 
 function mouseDragged() {
